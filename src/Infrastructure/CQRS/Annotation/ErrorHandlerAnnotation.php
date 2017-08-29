77b59a19a00c136e10d9ed7efd403fc216929749
Masiukevich Maksim <maksim.masiukevich@distate.dsnet>
2017-08-29 18:08:41 +0300

    fix error

--- src/Infrastructure/CQRS/Annotation/ErrorHandlerAnnotation.php
+++ src/Infrastructure/CQRS/Annotation/ErrorHandlerAnnotation.php
@@ -11,7 +11,7 @@
 
 declare(strict_types = 1);
 
-namespace Desperado\ConcurrencyFramework\Infrastructure\CQRS\Annotation\Service;
+namespace Desperado\ConcurrencyFramework\Infrastructure\CQRS\Annotation;
 
 use Desperado\ConcurrencyFramework\Domain\Annotation\AbstractAnnotation;
 
