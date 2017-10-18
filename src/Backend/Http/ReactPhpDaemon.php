43befecdbb4b846cb22cc6db3f75cca6220e0606
Masiukevich Maksim <maksim.masiukevich@distate.dsnet>
2017-10-18 17:19:00 +0300

    reactphp stub

---
+++ src/Backend/Http/ReactPhpDaemon.php
@@ -0,0 +1,205 @@
+<?php
+
+/**
+ * CQRS/Event Sourcing framework
+ *
+ * @author  Maksim Masiukevich <desperado@minsk-info.ru>
+ * @url     https://github.com/mmasiukevich
+ * @license MIT
+ * @license https://opensource.org/licenses/MIT
+ */
+
+declare(strict_types = 1);
+
+namespace Desperado\Framework\Backend\Http;
+
+use Desperado\Domain\ParameterBag;
+use Desperado\Framework\Application\ApplicationLogger;
+use EventLoop\EventLoop;
+use React\Http\Request;
+use React\Http\Response;
+use React\Http\Server as HttpServer;
+use React\Socket\Server as SimpleSocketServer;
+use React\Socket\SecureServer as SecuredSocketServer;
+use Desperado\Domain\DaemonInterface;
+use Desperado\Domain\EntryPointInterface;
+use Desperado\Infrastructure\Bridge\Router\RouterInterface;
+use React\Socket\ServerInterface;
+
+/**
+ * ReactPHP daemon
+ */
+class ReactPhpDaemon implements DaemonInterface
+{
+    protected const LOG_CHANNEL_NAME = 'reactPhp';
+    protected const DEFAULT_HOST = '0.0.0.0:0';
+    protected const DEFAULT_PORT = 1337;
+
+    /**
+     * Is https server
+     *
+     * @var bool
+     */
+    private $isSecured;
+
+    /**
+     * Pem file (ssl certificate) file path
+     *
+     * @var string|null
+     */
+    private $certificateFilePath;
+
+    /**
+     * Listen host
+     *
+     * @var string
+     */
+    private $listenHost;
+
+    /**
+     * Listen port
+     *
+     * @var int
+     */
+    private $listenPort;
+
+    /**
+     * Http router
+     *
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * React socket server
+     *
+     * @var ServerInterface
+     */
+    private $socketServer;
+
+    /**
+     * [
+     *     'isSecured'       => false,
+     *     'certificatePath' => null,
+     *     'host'            => '127.0.0.1',
+     *     'port'            => 1337
+     * ]
+     *
+     * @param array           $daemonOptions
+     * @param RouterInterface $router
+     *
+     * @throws \LogicException
+     */
+    public function __construct(array $daemonOptions, RouterInterface $router)
+    {
+        $parameters = new ParameterBag($daemonOptions);
+
+        $this->isSecured = (bool) $parameters->getAsInt('isSecured');
+        $this->certificateFilePath = $parameters->getAsString('certificatePath');
+        $this->listenHost = $parameters->getAsString('host', self::DEFAULT_HOST);
+        $this->listenPort = $parameters->getAsString('port', self::DEFAULT_PORT);
+        $this->router = $router;
+
+        if(
+            true === $this->isSecured &&
+            false === (true === \file_exists($this->certificateFilePath) && true === \is_readable($this->certificateFilePath))
+        )
+        {
+            throw new \LogicException('Invalid ssl certificate file path');
+        }
+
+        $this->initSignals();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function run(EntryPointInterface $entryPoint, array $clients = []): void
+    {
+        $this->socketServer = new SimpleSocketServer(EventLoop::getLoop());
+
+        if(true === $this->isSecured)
+        {
+            $this->socketServer = new SecuredSocketServer(
+                $this->socketServer,
+                EventLoop::getLoop(),
+                ['local_cert' => $this->certificateFilePath]
+            );
+        }
+
+        $httpServer = new HttpServer($this->socketServer);
+
+        $httpServer->on(
+            'request',
+            function(Request $request, Response $response)
+            {
+                $request->on(
+                    'data',
+                    function($data) use ($request, $response)
+                    {
+                        $this->handleRequest($request, $response, (string) $data);
+                    }
+                );
+
+                $this->handleRequest($request, $response);
+            }
+        );
+
+        $httpServer->on(
+            'error',
+            function(\Throwable $throwable, Response $response)
+            {
+                ApplicationLogger::throwable(self::LOG_CHANNEL_NAME, $throwable);
+
+                $response->writeHead(500);
+                $response->end('Internal error');
+            }
+        );
+
+        $this->socketServer->listen($this->listenPort, $this->listenHost);
+
+        EventLoop::getLoop()->run();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function stop(): void
+    {
+        $this->socketServer->shutdown();
+
+        EventLoop::getLoop()->stop();
+    }
+
+    /**
+     * Execute request
+     *
+     * @param Request  $request
+     * @param Response $response
+     * @param string   $bodyContent
+     *
+     * @return void
+     */
+    private function handleRequest(
+        Request $request,
+        Response $response,
+        string $bodyContent = ''
+    ): void
+    {
+        $response->writeHead(200);
+        $response->end('Success');
+    }
+
+    /**
+     * Init unix signals
+     *
+     * @return void
+     */
+    private function initSignals(): void
+    {
+        \pcntl_signal(\SIGINT, [$this, 'stop']);
+        \pcntl_signal(\SIGTERM, [$this, 'stop']);
+
+        \pcntl_async_signals(true);
+    }
+}
