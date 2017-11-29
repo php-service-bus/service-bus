67b7f7ad3eee6d3ef39ae22759c982a1a634a91d
Masiukevich Maksim <maksim.masiukevich@distate.dsnet>
2017-11-29 11:59:17 +0300

    init ci

---
+++ tests/InitTests.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * CQRS/Event Sourcing framework
+ *
+ * @author  Maksim Masiukevich <desperado@minsk-info.ru>
+ * @url     https://github.com/mmasiukevich
+ * @license MIT
+ * @license https://opensource.org/licenses/MIT
+ */
+
+declare(strict_types = 1);
+
+namespace Desperado\Framework\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @deprecated
+ */
+class InitTests extends TestCase
+{
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function someTest(): void
+    {
+        static::assertTrue(true);
+    }
+}
