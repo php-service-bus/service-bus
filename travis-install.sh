b7ae1578f508924993f757a84bdc13c48671374b
desperado <desperado@minsk-info.ru>
2018-08-17 17:22:32 +0300

    update travis config

--- travis-install.sh
+++ travis-install.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/m6w6/ext-pq;
-pushd ext-pq;
-phpize;
-./configure;
-make;
-make install;
-popd;
-echo "extension=pq.so" >> "$(php -r 'echo php_ini_loaded_file();')";
-
 apt-get install librabbitmq-dev;
 pecl install amqp
-echo "extension=pq.so" >> "$(php -r 'echo php_ini_loaded_file();')";
+echo "extension=amqp.so" >> "$(php -r 'echo php_ini_loaded_file();')";
