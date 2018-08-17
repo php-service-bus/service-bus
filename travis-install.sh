060e098e82421f2f8d7dda9a052d8c9063dfddb2
desperado <desperado@minsk-info.ru>
2018-08-17 17:26:30 +0300

    update travis config

--- travis-install.sh
+++ travis-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-apt-get install librabbitmq-dev;
-pecl install amqp
-echo "extension=amqp.so" >> "$(php -r 'echo php_ini_loaded_file();')";
+sudo apt-get install librabbitmq-dev -y;
+sudo pecl install amqp
+sudo echo "extension=amqp.so" >> "$(php -r 'echo php_ini_loaded_file();')";
