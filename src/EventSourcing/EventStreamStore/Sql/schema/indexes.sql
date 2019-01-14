f82297b2ff87272a06e980646135f026a54e6e61
desperado <desperado@minsk-info.ru>
2019-01-14 12:31:19 +0300

    change base snapshot fixtures

--- src/EventSourcing/EventStreamStore/Sql/schema/indexes.sql
+++ src/EventSourcing/EventStreamStore/Sql/schema/indexes.sql
@@ -2,3 +2,4 @@ CREATE UNIQUE INDEX IF NOT EXISTS event_store_snapshots_aggregate ON event_store
 CREATE UNIQUE INDEX IF NOT EXISTS event_store_stream_identifier ON event_store_stream (id, identifier_class);
 CREATE UNIQUE INDEX IF NOT EXISTS event_store_stream_events_playhead ON event_store_stream_events (stream_id, playhead) WHERE canceled_at IS NULL;
 CREATE INDEX IF NOT EXISTS event_store_stream_events_stream ON event_store_stream_events (id, stream_id);
+CREATE UNIQUE INDEX IF NOT EXISTS event_store_snapshots_identifier ON event_store_snapshots (id, aggregate_id_class);
